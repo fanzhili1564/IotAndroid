9b4d7d2e5197e99749a4dfad20b91d6d6b5d484d
fanzl <fzl1591564@163.com>
2018-07-02 20:57:36 +0800

    前轮驱动测试

--- IotCarApp/clientapp/src/main/java/com/car/smart/clientapp/SplashActivity.java
+++ IotCarApp/clientapp/src/main/java/com/car/smart/clientapp/SplashActivity.java
@@ -2,6 +2,12 @@ package com.car.smart.clientapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManager;
+
+import java.io.IOException;
 
 /**
  * Skeleton of an Android Things activity.
@@ -24,9 +30,55 @@ import android.os.Bundle;
  */
 public class SplashActivity extends Activity {
 
+    private Gpio INPUT_ENA_A;
+    private Gpio INPUT_ENB_A;
+    private Gpio INPUT_IN1_A;
+    private Gpio INPUT_IN2_A;
+    private Gpio INPUT_IN3_A;
+    private Gpio INPUT_IN4_A;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        initGpio();
+    }
+
+    private void initGpio() {
+        PeripheralManager pioService = PeripheralManager.getInstance();
+        try {
+            INPUT_IN1_A = pioService.openGpio("BCM17");
+            INPUT_IN2_A = pioService.openGpio("BCM18");
+            INPUT_IN3_A = pioService.openGpio("BCM27");
+            INPUT_IN4_A = pioService.openGpio("BCM22");
+            INPUT_ENA_A = pioService.openGpio("BCM23");
+            INPUT_ENB_A = pioService.openGpio("BCM24");
+
+            INPUT_ENA_A.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
+            INPUT_ENB_A.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
+
+            INPUT_IN1_A.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
+            INPUT_IN2_A.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            INPUT_IN3_A.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
+            INPUT_IN4_A.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+
+            Gpio INPUT_IN1_B = pioService.openGpio("BCM6");
+            Gpio INPUT_IN2_B = pioService.openGpio("BCM13");
+            Gpio INPUT_IN3_B = pioService.openGpio("BCM16");
+            Gpio INPUT_IN4_B = pioService.openGpio("BCM19");
+            Gpio INPUT_ENA_B = pioService.openGpio("BCM26");
+            Gpio INPUT_ENB_B = pioService.openGpio("BCM20");
+
+            INPUT_ENA_B.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
+            INPUT_ENB_B.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
+
+            INPUT_IN1_B.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
+            INPUT_IN2_B.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            INPUT_IN3_B.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
+            INPUT_IN4_B.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+        } catch (IOException e) {
+            Log.e("SplashActivity", e.getMessage());
+        }
     }
 }
